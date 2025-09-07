3243f696f54cc67428176521c48157f01aa9843f
YashIngle21 <yashingle1729@gmail.com>
2025-09-07 20:37:05 +0000

    Added some variables and structs

--- scripts/Decentralized_sub_service.sol
+++ scripts/Decentralized_sub_service.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.28;
 
 error InsufficientFunds(uint256 price, uint256 price_provided);
+error NotProvider(address provider);
+error PlanNotExists(uint64 planId);
 
 contract Decentralized_sub_service{
 
@@ -11,38 +13,59 @@ contract Decentralized_sub_service{
         address provider;
     }
 
+
+    struct subscription{
+        uint256 expiryTimeStamp;
+        uint256 startTimeStamp;
+        uint256 totalAmountPaid;
+    }
+
     // uint32 private id_counter;
 
-    mapping (uint64 => plan) public Plans;
-    mapping (address => uint256) private subscribers;
 
-    function creatPlan(uint32 id, uint256 _price, uint32 _duration) external {
-        Plans[id] = plan(_price, _duration, msg.sender);
+    mapping(address=> bool) public isProvider;
+    mapping (uint64 => plan) public Plans;
+    mapping(uint256 => mapping (address => subscription)) public subscriptions;
+
+    modifier onlyProvider(){
+        if(!isProvider[msg.sender]){
+            revert NotProvider(msg.sender);
+        }
+        _;
+    }
+    modifier planExists(uint64 planId){
+        if(Plans[planId].provider == address(0)){
+            revert PlanNotExists(planId);
+        }
+        _;
+    }
+    function creatPlan(uint32 planId, uint256 _price, uint32 _duration) external planExists(planId){
+        Plans[planId] = plan(_price, _duration, msg.sender);
     }
 
     function subscribe(uint64 planId) external payable {
         if(msg.value < Plans[planId].price){
             revert InsufficientFunds(Plans[planId].price, msg.value);
         }
-        subscribers[msg.sender] =block.timestamp + Plans[planId].duration;
+        
     }
 
     function renew(uint64 planId) external payable {
         if(msg.value < Plans[planId].price){
             revert InsufficientFunds(Plans[planId].price, msg.value);
         }
-        subscribers[msg.sender] = subscribers[msg.sender] + Plans[planId].duration;
+        
     }
 
-    function isActive() external view returns(bool){
-        if(subscribers[msg.sender] > block.timestamp){
+    function isActive(uint64 planId) external view returns(bool){
+        if(subscriptions[planId][msg.sender].expiryTimeStamp > block.timestamp){
             return true;
         }else{
             return false;
         }
     }
 
-    function withdrawFunds() external {
+    function withdrawFunds() external onlyProvider(){
         
     }
     
